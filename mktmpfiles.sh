cf35a560099ba74f592f1f9f6af192cdf5029d3e
Daniel <Chang254@mail.chapman.edu>
2018-02-02 01:53:16 +0000

    Added if statements to more accurately name created files.  Also added comments to explain elementary steps of the program.

--- mktmpfiles.sh
+++ mktmpfiles.sh
@@ -1,4 +1,23 @@
+#Daniel, Devon, and Myranda
+#PHYS220
+#CW-01
+
+#Creates and goes to testfiles directory
 mkdir ./testfiles/
 cd testfiles
-for i in $(seq 100); do touch ./file$i.tmp; done
-cd .. 
+
+#Makes testfiles 001-100
+for i in $(seq 100); do
+    if [ $i -lt 10 ]; then
+        touch ./file00$i.tmp
+    elif [ $i -ge 10 ]; then
+        if [ $i -eq 100 ]; then
+            touch ./file$i.tmp
+        else
+            touch ./file0$i.tmp
+        fi
+    fi
+done
+
+#Changes back to the original directory
+cd ..
